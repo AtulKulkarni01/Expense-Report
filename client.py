a7de505824730a493e62fdd9a0bc31ecf47ee3c2
AtulKulkarni01 <atulkulkarni2002@gmail.com>
2024-07-08 20:17:55 +0530

    First Commit

---
+++ client.py
@@ -0,0 +1,5 @@
+from main import expense_report
+
+text = expense_report()
+
+text.get_expense("bill2.jpg")
---
+++ main.py
@@ -0,0 +1,57 @@
+from OCR import OCRHandler
+import spacy
+import regex as re
+
+class expense_report:
+    def __init__(self) -> None:
+        self.ocr = OCRHandler()
+        self.nlp = spacy.load("en_core_web_trf")
+
+
+    def get_expense(self, image_path):
+        result = self.ocr.read_text_from_image(image_path=image_path)
+
+        doc = self.nlp(" ".join(result))
+        joined_text = " ".join(result)
+
+        company_name = self.get_company_name(doc)
+        date = self.get_date(joined_text)
+        print("THis is the date :", set(date).pop())
+
+        price = self.get_price(joined_text)
+        print("this is the total amount : ", price)
+
+    def get_company_name(self, doc):
+        
+        entities = [(ent.text, ent.label_) for ent in doc.ents]
+
+        # Print the named entities
+        for entity, label in entities:
+            if label == 'ORG':
+                print(f"Company: {entity}")
+                break
+
+    def get_date(self, text):
+    
+        date_pattern = r'\b\d{1,2}/\d{1,2}/\d{2}\b'
+
+        # Find all matches in the text
+        dates = re.findall(date_pattern, text)
+
+        # Print the extracted dates
+        print("Dates:", dates)
+        return dates
+    
+    def get_price(self, text):
+        pattern = r'\b*\d+\.\d{2}\b'
+
+        # Find all matches in the string
+        prices = re.findall(pattern, text)
+
+        maxi = prices[0]
+
+        for i in range(1, len(prices)):
+            maxi = max(prices[i], maxi)
+
+        return maxi
+
